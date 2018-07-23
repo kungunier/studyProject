fde69d19401450bf9e3190a90d987a96436a857e
liuhailong <wb_liuhailong@zhongan.com>
2018-07-23 16:43:24 +0800

    student

---
+++ src/bean/Students.java
@@ -0,0 +1,17 @@
+package bean;
+
+public class Students {
+
+    private int id;
+    private String name;
+    private int age;
+    private String kemu;
+    private String zhuanye;
+    private String school;
+    private String banji;
+
+    public String toString(){
+        return name+"\n"+age+"\n"+kemu+"\n"+zhuanye+"\n"+school+"\n"+banji;
+    }
+
+}
